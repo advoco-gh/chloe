b44ea221c02e2efe27611d14461b35a53e36a053
carloodq <dallaquerciacarlo@gmail.com>
2022-01-17 12:05:06 +0100

    adding intents

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,29 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from typing import Text, List, Optional
+
+
+class Validate_already_have_agent(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_already_have_agent"
+
+    async def required_slots(
+            self,
+            slots_mapped_in_domain: List[Text],
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+        if tracker.get_slot("want_to_continue"):
+            slots_mapped_in_domain.append("reply_to_manager_advice")
+        else:
+            slots_mapped_in_domain.append("goodbye")
+        return slots_mapped_in_domain
+
