71c96b41a3090b489ae20183aee94b16d5a27990
carloodq <dallaquerciacarlo@gmail.com>
2022-01-19 22:35:11 +0100

    adding the callback another party

--- actions/actions.py
+++ actions/actions.py
@@ -26,12 +26,16 @@
 #
 #         return []
 
-from typing import Text, List, Any, Dict
+from typing import Text, List, Any, Dict, Optional
+
 
 from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
-from typing import Text, List, Optional
+
+from rasa_sdk.events import FollowupAction, ActionExecuted
+
+from rasa_sdk import Action, Tracker
 
 
 class Validate_already_have_agent(FormValidationAction):
@@ -50,4 +54,37 @@ class Validate_already_have_agent(FormValidationAction):
         else:
             slots_mapped_in_domain.append("goodbye")
         return slots_mapped_in_domain
+
+
+
+
+class action_chloe(Action):
+
+    def name(self) -> Text:
+        return "action_chloe"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        a = tracker.events
+
+        only_actions = []
+        only_intents = []
+
+        for x in a:
+            if x['event'] == 'user':
+                only_intents.append(x['parse_data']['intent']['name'])
+            elif x['event'] == 'bot':
+                try:
+                    only_actions.append(x['metadata']['utter_action'])
+                except:
+                    pass
+
+        if only_intents[-1] == "speak_twice":
+            reply = only_actions[-1] 
+
+        dispatcher.utter_message(response = reply)
+        return []
+
 
