77783b621ce0d973dd92a826da2adf421e89f2fc
carloodq <dallaquerciacarlo@gmail.com>
2022-02-16 15:50:07 +0800

    adding conversion form, rule for morning age etc after greeting, custom action for 2 step forms

--- actions/actions.py
+++ actions/actions.py
@@ -39,6 +39,26 @@ from rasa_sdk import Action, Tracker
 
 
 
+class Validate_conversion_form(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_conversion_form"
+
+    async def required_slots(
+            self,
+            slots_mapped_in_domain: List[Text],
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+        if not tracker.get_slot("app_call_time"):
+            slots_mapped_in_domain.append("goodbye")
+
+        if not tracker.get_slot("age_info"):
+            slots_mapped_in_domain.append("goodbye")
+
+        return slots_mapped_in_domain
+        
+        
 class Validate_already_have_agent(FormValidationAction):
     def name(self) -> Text:
         return "validate_already_have_agent"
@@ -77,6 +97,44 @@ class Validate_objection_already_have_PA_plan(FormValidationAction):
 
 
 
+
+class action_trigger_conversion_form(Action):
+
+    def name(self) -> Text:
+        return "action_trigger_conversion_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        a = tracker.events
+
+        only_actions = []
+        only_intents = []
+
+        for x in a:
+            if x['event'] == 'user':
+                only_intents.append(x['parse_data']['intent']['name'])
+            elif x['event'] == 'bot':
+                try:
+                    only_actions.append(x['metadata']['utter_action'])
+                except:
+                    pass
+
+        # if only_intents[-1] == "speak_twice":
+        #     reply = only_actions[-1] 
+        if only_actions[-1] == 'utter_chloe_greeting':
+            dispatcher.utter_message(response = 'utter_chloe_greeting_repeat')
+            return []
+
+        else:
+            rules =  {'already_have_agent': 'already_have_agent',
+                        'objection_already_have_PA_plan': 'objection_already_have_PA_plan',
+                        'question_related_to_age': 'question_related_to_age'}
+            reply_action = rules[only_intents[-1]]
+            return [FollowupAction(name=reply_action)]
+        
+        
 
 class action_chloe(Action):
 
@@ -131,25 +189,44 @@ class action_rule_only_once(Action):
                 except:
                     pass
 
-
-        reply = ''
-        for intent in ['fees_question' , 'question_related_to_scam', 'question_related_to_numbers', 'not_singaporean', 'pa_plan_coverage' ]:
-            if only_intents.count(intent) > 1:
-                reply = 'utter_chloe_goodbye'
-
         
-        if reply != 'utter_chloe_goodbye':
-            rules = {'fees_question' :
-                    'utter_chloe_pa_fees',
-                    'question_related_to_scam':
-                    'utter_chloe_scam',
-                    'question_related_to_numbers':
-                    'utter_chloe_number_info',
-                    'not_singaporean':
-                    'utter_chloe_not_local',
-                    'pa_plan_coverage':
-                    'utter_chloe_pa_coverage'}
-            reply = rules[only_intents[-1]]
+        if only_actions[-1] == 'utter_chloe_greeting' and only_intents[-1] != 'question_related_to_numbers ':
+            reply = 'utter_chloe_greeting_repeat'
+        
+        else:
+            reply = ''
+            for intent in ['fees_question' , 'question_related_to_scam', 'question_related_to_numbers', 'not_singaporean', 'pa_plan_coverage' ]:
+                if only_intents.count(intent) > 1:
+                    reply = 'utter_chloe_goodbye'
+
+            
+            if reply != 'utter_chloe_goodbye':
+                rules = {'fees_question' :
+                        'utter_chloe_pa_fees',
+                        'question_related_to_scam':
+                        'utter_chloe_scam',
+                        'question_related_to_numbers':
+                        'utter_chloe_number_info',
+                        'not_singaporean':
+                        'utter_chloe_not_local',
+                        'pa_plan_coverage':
+                        'utter_chloe_pa_coverage',
+                        'manager_question':
+                        'utter_chloe_manager_introduction',
+                        'question_speak_chinese':
+                        'utter_chloe_language_barrier',
+                        'objection_i_am_agent':
+                        'utter_chloe_goodbye',
+                        'morning':
+                        'utter_chloe_greeting_repeat',
+                        'afternoon':
+                        'utter_chloe_greeting_repeat',
+                        'evening':
+                        'utter_chloe_greeting_repeat',
+                        'age':
+                        'utter_chloe_greeting_repeat',
+                        }
+                reply = rules[only_intents[-1]]
 
 
 
