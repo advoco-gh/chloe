6ab433ce28e82a360631decfcf2dcbc3e40b3920
msahab <mrsahab@outlook.com>
2022-05-18 16:57:11 +0500

    fixed the issues

--- actions/actions.py
+++ actions/actions.py
@@ -38,7 +38,6 @@ from rasa_sdk.events import FollowupAction, ActionExecuted
 from rasa_sdk import Action, Tracker
 
 
-
 class Validate_conversion_form(FormValidationAction):
     def name(self) -> Text:
         return "validate_conversion_form"
@@ -57,8 +56,8 @@ class Validate_conversion_form(FormValidationAction):
             slots_mapped_in_domain.append("goodbye")
         print('{}, slots: {}'.format(self.name(), slots_mapped_in_domain))
         return slots_mapped_in_domain
-        
-        
+
+
 class Validate_already_have_agent(FormValidationAction):
     def name(self) -> Text:
         return "validate_already_have_agent"
@@ -87,17 +86,14 @@ class Validate_objection_already_have_PA_plan(FormValidationAction):
             tracker: "Tracker",
             domain: "DomainDict",
     ) -> Optional[List[Text]]:
-            if tracker.get_slot("alternative_plan"):
-                return slots_mapped_in_domain
-            
-            else:
-                slots_mapped_in_domain.append("goodbye")
-                print('{}, slots: {}'.format(self.name(), slots_mapped_in_domain))
+        if tracker.get_slot("alternative_plan"):
+            return slots_mapped_in_domain
 
-                return slots_mapped_in_domain
+        else:
+            slots_mapped_in_domain.append("goodbye")
+            print('{}, slots: {}'.format(self.name(), slots_mapped_in_domain))
 
-
-
+            return slots_mapped_in_domain
 
 
 class action_trigger_conversion_form(Action):
@@ -124,23 +120,24 @@ class action_trigger_conversion_form(Action):
                     pass
 
         # if only_intents[-1] == "speak_twice":
-        #     reply = only_actions[-1] 
+        #     reply = only_actions[-1]
         if only_actions[-1] == 'utter_chloe_greeting':
-            print('{}, intent: {}, action: {}, reply; {}'.format(self.name(), only_intents[-1], only_actions[-1], reply))
+            print('{}, intent: {}, action: {}, reply; {}'.format(
+                self.name(), only_intents[-1], only_actions[-1], reply))
 
-            dispatcher.utter_message(response = 'utter_chloe_greeting_repeat')
+            dispatcher.utter_message(response='utter_chloe_greeting_repeat')
             return []
 
         else:
-            rules =  {'already_have_agent': 'already_have_agent',
-                        'objection_already_have_PA_plan': 'objection_already_have_PA_plan',
-                        'question_related_to_age': 'question_related_to_age'}
+            rules = {'already_have_agent': 'already_have_agent',
+                     'objection_already_have_PA_plan': 'objection_already_have_PA_plan',
+                     'question_related_to_age': 'question_related_to_age'}
             reply_action = rules[only_intents[-1]]
-            print('{}, intent: {}, action: {}, reply; {}'.format(self.name(), only_intents[-1], only_actions[-1], reply))
+            print('{}, intent: {}, action: {}, reply; {}'.format(
+                self.name(), only_intents[-1], only_actions[-1], reply_action))
 
             return [FollowupAction(name=reply_action)]
-        
-        
+
 
 class action_chloe(Action):
 
@@ -166,13 +163,14 @@ class action_chloe(Action):
                     pass
 
         if only_intents[-1] == "speak_twice":
-            reply = only_actions[-1] 
-        print('{}, intent: {}, action: {}, reply; {}'.format(self.name(), only_intents[-1], only_actions[-1], reply))
- 
-        dispatcher.utter_message(response = reply)
+            reply = only_actions[-1]
+        print('{}, intent: {}, action: {}, reply; {}'.format(
+            self.name(), only_intents[-1], only_actions[-1], reply))
+
+        dispatcher.utter_message(response=reply)
         return []
-        
-        
+
+
 class action_rule_only_once(Action):
 
     def name(self) -> Text:
@@ -196,51 +194,49 @@ class action_rule_only_once(Action):
                 except:
                     pass
 
-        
         if only_actions[-1] == 'utter_chloe_greeting' and only_intents[-1] != 'question_related_to_numbers ':
             reply = 'utter_chloe_greeting_repeat'
-        
+
         else:
             reply = ''
-            for intent in ['fees_question' , 'question_related_to_scam', 'question_related_to_numbers', 'not_singaporean', 'pa_plan_coverage' ]:
+            for intent in ['fees_question', 'question_related_to_scam', 'question_related_to_numbers', 'not_singaporean', 'pa_plan_coverage']:
                 if only_intents.count(intent) > 1:
                     reply = 'utter_chloe_goodbye'
 
-            
             if reply != 'utter_chloe_goodbye':
-                rules = {'fees_question' :
-                        'utter_chloe_pa_fees',
-                        'question_related_to_scam':
-                        'utter_chloe_scam',
-                        'question_related_to_numbers':
-                        'utter_chloe_number_info',
-                        'not_singaporean':
-                        'utter_chloe_not_local',
-                        'pa_plan_coverage':
-                        'utter_chloe_pa_coverage',
-                        'manager_question':
-                        'utter_chloe_manager_introduction',
-                        'question_speak_chinese':
-                        'utter_chloe_language_barrier',
-                        'objection_i_am_agent':
-                        'utter_chloe_goodbye',
-                        'morning':
-                        'utter_chloe_greeting_repeat',
-                        'afternoon':
-                        'utter_chloe_greeting_repeat',
-                        'evening':
-                        'utter_chloe_greeting_repeat',
-                        'date_day':
-                        'utter_chloe_greeting_repeat',
-                        'age':
-                        'utter_chloe_greeting_repeat',
-                        }
+                rules = {'fees_question':
+                         'utter_chloe_pa_fees',
+                         'question_related_to_scam':
+                         'utter_chloe_scam',
+                         'question_related_to_numbers':
+                         'utter_chloe_number_info',
+                         'not_singaporean':
+                         'utter_chloe_not_local',
+                         'pa_plan_coverage':
+                         'utter_chloe_pa_coverage',
+                         'manager_question':
+                         'utter_chloe_manager_introduction',
+                         'question_speak_chinese':
+                         'utter_chloe_language_barrier',
+                         'objection_i_am_agent':
+                         'utter_chloe_goodbye',
+                         'morning':
+                         'utter_chloe_greeting_repeat',
+                         'afternoon':
+                         'utter_chloe_greeting_repeat',
+                         'evening':
+                         'utter_chloe_greeting_repeat',
+                         'date_day':
+                         'utter_chloe_greeting_repeat',
+                         'age':
+                         'utter_chloe_greeting_repeat',
+                         }
                 reply = rules[only_intents[-1]]
 
+        print('{}, intent: {}, action: {}, reply; {}'.format(
+            self.name(), only_intents[-1], only_actions[-1], reply))
 
-        print('{}, intent: {}, action: {}, reply; {}'.format(self.name(), only_intents[-1], only_actions[-1], reply))
-
-        dispatcher.utter_message(response = reply)
+        dispatcher.utter_message(response=reply)
         return []
 
 
@@ -266,19 +262,17 @@ class action_busy_reply(Action):
                     only_actions.append(x['metadata']['utter_action'])
                 except:
                     pass
-        
-
 
         replies = {'pa_plan_coverage': 'utter_chloe_pa_coverage',
-                'objection_already_have_PA_plan': 'chloe_had_a_pa_plan',
-                'nlu_fallback': 'utter_fallback',
-                'fees_question': 'chloe_pa_fees',
-                'question_related_to_scam': 'utter_chloe_scam',
-                'call_back_to_another_party': 'chloe_call _back_other',
-                'question_related_to_numbers':'chloe_number_info',
-                'looking_for':'chloe_looking_for'
-                }
-    
+                   'objection_already_have_PA_plan': 'chloe_had_a_pa_plan',
+                   'nlu_fallback': 'utter_fallback',
+                   'fees_question': 'chloe_pa_fees',
+                   'question_related_to_scam': 'utter_chloe_scam',
+                   'call_back_to_another_party': 'chloe_call _back_other',
+                   'question_related_to_numbers': 'chloe_number_info',
+                   'looking_for': 'chloe_looking_for'
+                   }
+
         print('action_busy_reply', only_intents, only_actions)
         for intent, ans in replies.items():
             if intent == only_intents[-1]:
@@ -288,6 +282,6 @@ class action_busy_reply(Action):
                 reply = 'utter_chloe_goodbye'
 
         if only_intents.count('busy') > 1:
-                reply = 'utter_chloe_goodbye'
-        dispatcher.utter_message(response = reply)
+            reply = 'utter_chloe_goodbye'
+        dispatcher.utter_message(response=reply)
         return []
